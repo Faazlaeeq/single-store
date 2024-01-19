7e524d87139eb0da3e43692740c64f1636037c87
FaazLaeeq <faazlaeeq@gmail.com>
2024-01-19 17:26:47 +0500

    Add delivery address, product item, and promo code sections to order detail screen

--- lib/screens/Order/order_detail_screen.dart
+++ lib/screens/Order/order_detail_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:single_store/Data/data.dart';
 import 'package:single_store/theme/my_colors.dart';
 import 'package:single_store/theme/sizes.dart';
+import 'package:single_store/widgets/common_widgets.dart';
 import 'package:single_store/widgets/my_appbar_widget.dart';
 
 class OrderDetailScreen extends StatelessWidget {
@@ -16,15 +18,21 @@ class OrderDetailScreen extends StatelessWidget {
     ["Country calling code:", "+1"],
     ["Country:", "United States"],
   ];
+
+  final productItem = Data().itemsProduct;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: MyAppbar(
-          scaffoldKey: scaffoldKey,
-          showTrailingIcon: false,
-        ),
+            scaffoldKey: scaffoldKey,
+            showTrailingIcon: false,
+            leadingIcon: "assets/icons/arrow-smooth-left.png",
+            onpressed: () => Navigator.of(context).pop()),
         body: Column(
           children: [
+            Padding(
+                padding: EdgeInsets.symmetric(horizontal: padding5),
+                child: CommonWidgets.headingText(context, "Delivery Address")),
             Container(
               padding: const EdgeInsets.all(padding3),
               margin: const EdgeInsets.all(padding5),
@@ -56,6 +64,68 @@ class OrderDetailScreen extends StatelessWidget {
                     ],
                   );
                 }),
+              ),
+            ),
+            Padding(
+                padding: EdgeInsets.symmetric(horizontal: padding5),
+                child: CommonWidgets.headingText(context, "Product Item")),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: padding3),
+              child: CommonWidgets.customListTile(
+                context: context,
+                imageUrl: productItem[0].image,
+                name: productItem[0].name,
+                subtitle: productItem[0].description,
+                description: "\$${productItem[0].price.toString()}",
+                crossAxisAlignment: CrossAxisAlignment.center,
+                subtitleStyle: Theme.of(context)
+                    .textTheme
+                    .labelSmall!
+                    .copyWith(fontSize: 9),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: padding3),
+              child: CommonWidgets.customListTile(
+                context: context,
+                imageUrl: productItem[1].image,
+                name: productItem[1].name,
+                subtitle: productItem[1].description,
+                description: "\$${productItem[1].price.toString()}",
+                crossAxisAlignment: CrossAxisAlignment.center,
+                subtitleStyle: Theme.of(context)
+                    .textTheme
+                    .labelSmall!
+                    .copyWith(fontSize: 9),
+              ),
+            ),
+            Padding(
+                padding: EdgeInsets.symmetric(horizontal: padding5),
+                child: CommonWidgets.headingText(context, "Promo Code")),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: padding3),
+              child: CommonWidgets.customListTile(
+                context: context,
+                imageUrl: "assets/icons/promocode.png",
+                name: "Add Promo Code",
+                subtitle: "gotit2021",
+                crossAxisAlignment: CrossAxisAlignment.start,
+                subtitleStyle: Theme.of(context)
+                    .textTheme
+                    .labelSmall!
+                    .copyWith(fontSize: 9),
+                heightImg: 40,
+                widthImg: 40,
+              ),
+            ),
+            Spacer(),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: padding5),
+              child: CommonWidgets.listTileWithButton(
+                context: context,
+                buttonText: "Place Order",
+                subtitle:
+                    "\$${(productItem[0].price + productItem[1].price).toStringAsFixed(2)}",
               ),
             )
           ],
--- lib/widgets/common_widgets.dart
+++ lib/widgets/common_widgets.dart
@@ -46,4 +46,140 @@ class CommonWidgets {
       ),
     );
   }
+
+  static Widget customListTile({
+    required BuildContext context,
+    String? imageUrl,
+    String? name,
+    String? subtitle,
+    Widget? titleEnd,
+    String? description,
+    MainAxisAlignment? mainAxisAlignment,
+    CrossAxisAlignment? crossAxisAlignment,
+    TextStyle? subtitleStyle,
+    double? heightImg,
+    double? widthImg,
+  }) {
+    return Container(
+        margin: const EdgeInsets.symmetric(
+            horizontal: padding2, vertical: padding2),
+        padding: const EdgeInsets.symmetric(
+            horizontal: padding1, vertical: padding1),
+        decoration: BoxDecoration(
+          color: MyColors.secondaryColor,
+          borderRadius: BorderRadius.circular(13),
+          boxShadow: const [MyColors.spreadedShadow],
+        ),
+        child: Row(
+          mainAxisAlignment: mainAxisAlignment ?? MainAxisAlignment.start,
+          crossAxisAlignment: crossAxisAlignment ?? CrossAxisAlignment.start,
+          children: [
+            imageUrl == null
+                ? const SizedBox()
+                : Container(
+                    alignment: Alignment.topCenter,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        image: DecorationImage(
+                            image: AssetImage(imageUrl), fit: BoxFit.cover)),
+                    height: heightImg ?? 60,
+                    width: widthImg ?? 60,
+                  ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                    horizontal: padding2, vertical: 0),
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(name ?? "",
+                                style: Theme.of(context).textTheme.titleSmall!),
+                            SizedBox(child: titleEnd)
+                          ],
+                        ),
+                      ),
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          subtitle ?? "",
+                          style: subtitleStyle ??
+                              Theme.of(context).textTheme.labelSmall!.copyWith(
+                                  fontWeight: FontWeight.bold,
+                                  color: MyColors.subtitleColor2),
+                        ),
+                      ),
+                      description == null
+                          ? const SizedBox()
+                          : Align(
+                              alignment: Alignment.centerLeft,
+                              child: Text(
+                                description,
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .labelSmall!
+                                    .copyWith(color: MyColors.primaryColor),
+                              ),
+                            ),
+                    ]),
+              ),
+            ),
+          ],
+        ));
+  }
+
+  static Widget listTileWithButton(
+      {required BuildContext context,
+      String? iconUrl,
+      String? title,
+      String? subtitle,
+      String? buttonText,
+      VoidCallback? onPressed}) {
+    return ListTile(
+      contentPadding: const EdgeInsets.symmetric(horizontal: 0),
+      title: Text(title ?? "Total Price",
+          style: Theme.of(context).textTheme.labelSmall!.copyWith(fontSize: 9)),
+      subtitle: subtitle == null
+          ? const SizedBox()
+          : Text(
+              subtitle,
+              style:
+                  Theme.of(context).textTheme.bodyLarge!.copyWith(fontSize: 18),
+            ),
+      trailing: ElevatedButton(
+        onPressed: onPressed ??
+            () {
+              Navigator.of(context).pushNamed(RoutesManager.cart);
+            },
+        child: SizedBox(
+          width: width(context) * .35,
+          height: 50,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              iconUrl == null
+                  ? const SizedBox()
+                  : ImageIcon(
+                      AssetImage(iconUrl),
+                      size: 16,
+                    ),
+              const SizedBox(
+                width: 10,
+              ),
+              Text(
+                buttonText ?? "Add to Cart",
+                style: Theme.of(context)
+                    .textTheme
+                    .titleSmall!
+                    .copyWith(color: MyColors.secondaryColor),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
